0049192dd6d73436163a2e866bca2be70f3d1dc7
Lulalaby <aiko@aiko-it-systems.eu>
2020-05-08 00:39:04 +0200

    Added Test Mail Endpoint

---
+++ dashboard/src/Controller/MailerController.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class MailerController extends AbstractController
+{
+    /**
+     * @Route("/email")
+     */
+    public function sendEmail(MailerInterface $mailer)
+    {
+        $email = (new Email())
+            ->from(Address::fromString('Meek Test <meek.moe@systems.aiko-it-systems.eu>'))
+            ->to('aiko@ragebears.de')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            ->replyTo('aiko@aiko-it-systems.eu')
+            ->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $mailer->send($email);
+
+        return new Response(
+            '<html><body>Success</body></html>'
+        );
+        
+    }
+}
